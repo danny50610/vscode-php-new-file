1192ac2c4557d31304904e85bea810fa7971b4a3
Chan, Danny <danny50610@yahoo.com.tw>
2023-06-18 20:31:13 +0800

    create php file

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { join } from 'path';
+import { Position, Uri, WorkspaceEdit, window, workspace } from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -13,10 +15,44 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-php-new-file.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-php-new-file!');
+	let disposable = vscode.commands.registerCommand('vscode-php-new-file.create-php-file', async (uri: Uri) => {
+		// TODO: window.withProgress
+
+		const itemPath = await window.showInputBox({
+			prompt: 'Enter the path of the resources, relative to current folder',
+			value: '/'
+		});
+
+		if (itemPath === undefined) {
+			return; // TODO
+		}
+
+		const edit = new WorkspaceEdit();
+		const filepath = join(uri.fsPath, itemPath);
+
+		const newFileUri = Uri.file(filepath);
+        edit.createFile(newFileUri, {
+            overwrite: false,
+            ignoreIfExists: true
+        });
+
+		edit.insert(newFileUri, new Position(0, 0), '<?php'); // TODO: end new line
+
+		await workspace.applyEdit(edit);
+
+		// save documents and open the first
+		await Promise.all(
+			edit.entries().map(async ([uri], i) => {
+				const doc = workspace.textDocuments.find((t) => t.uri.path === uri.path);
+				if (doc) {
+					await doc?.save();
+					if (i === 0) {
+						await workspace.openTextDocument(uri);
+						await window.showTextDocument(doc);
+					}
+				}
+			})
+		);
 	});
 
 	context.subscriptions.push(disposable);
