a9826ad6aee461ae4b0ce3309f239a21826c634c
Chan, Danny <danny50610@yahoo.com.tw>
2023-06-18 20:32:02 +0800

    TODO

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,11 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { join } from 'path';
 import { Position, Uri, WorkspaceEdit, window, workspace } from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// https://github.com/sveltejs/language-tools/blob/master/packages/svelte-vscode/src/sveltekit/generateFiles/index.ts
+
+
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-php-new-file" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('vscode-php-new-file.create-php-file', async (uri: Uri) => {
 		// TODO: window.withProgress
 
@@ -58,5 +49,4 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
